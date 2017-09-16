c070a6d8ce66456a5ceb9414d138f57da46736eb
LarsM <lam@cphbusiness.dk>
2017-09-16 12:12:19 +0200

    Added first webScraper ex

---
+++ src/main/java/day2/webscraper/TagCounter.java
@@ -0,0 +1,58 @@
+package day2.webscraper;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class TagCounter {
+  String url;
+  String title;
+  int h1Count,h2Count, divCount, bodyCount;
+  TagCounter(String url){
+    this.url = url;
+  }
+  
+  /*
+  Connect to the URL and count the number of h1, h2, div and body Tags
+  */
+  public void run(){
+    Document doc;
+    try {
+      doc = Jsoup.connect(url).get();
+      // get page title
+      this.title = doc.title();
+      Elements h1s = doc.select("h1");
+      this.h1Count = h1s.size();
+      Elements h2s = doc.select("h2");
+      this.h2Count = h2s.size();
+      Elements divs = doc.select("div");
+      this.divCount = divs.size();
+      Elements bodys = doc.select("body");
+      this.bodyCount = bodys.size();
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public int getH1Count() {
+    return h1Count;
+  }
+
+  public int getH2Count() {
+    return h2Count;
+  }
+
+  public int getDivCount() {
+    return divCount;
+  }
+
+  public int getBodyCount() {
+    return bodyCount;
+  }
+  
+}
---
+++ src/main/java/day2/webscraper/Tester.java
@@ -0,0 +1,26 @@
+package day2.webscraper;
+
+public class Tester {
+  
+  public static void main(String[] args) {
+    TagCounter tc1 = new TagCounter("http://www.fck.dk");
+    tc1.run();
+    TagCounter tc2= new TagCounter("http://www.google.com");
+    tc2.run();
+    TagCounter tc3= new TagCounter("http://cphbusiness.mrooms.net/");
+    tc3.run();
+    
+    System.out.println("Title: "+tc1.getTitle());
+    System.out.println("Div's: "+tc1.getDivCount());
+    System.out.println("Body's: "+tc1.getBodyCount());         
+    
+    
+    System.out.println("Title: "+tc2.getTitle());
+    System.out.println("Div's: "+tc2.getDivCount());
+    System.out.println("Body's: "+tc2.getBodyCount());   
+    
+    System.out.println("Title: "+tc3.getTitle());
+    System.out.println("Div's: "+tc3.getDivCount());
+    System.out.println("Body's: "+tc3.getBodyCount());         
+  }
+}
